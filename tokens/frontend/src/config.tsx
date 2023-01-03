9f98f18b5245edd9ef8cc8dc645eb7a6e0a6cd63
paha <pahany4@mail.ru>
2023-01-03 18:03:49 +0700

    react

--- tokens/frontend/src/config.tsx
+++ tokens/frontend/src/config.tsx
@@ -3,9 +3,11 @@ import Session from "supertokens-auth-react/recipe/session";
 
 export const SuperTokensConfig = {
     appInfo: {
-        appName: "SuperTokens Demo App",
+        appName: "tokens",
         apiDomain: "http://localhost:3001",
         websiteDomain: "http://localhost:3000",
+        apiBasePath: "/auth",
+        websiteBasePath: "/auth"
     },
     // recipeList contains all the modules that you want to
     // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
